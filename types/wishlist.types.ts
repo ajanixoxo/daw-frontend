36bd675a16568d535139a40f27e2baeea391d514
Pramendra Singh <pramendrasinghravi@gmail.com>
2025-12-21 15:59:14 +0530

    added changes

--- types/wishlist.types.ts
+++ types/wishlist.types.ts
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 import { IProduct } from "./product.types";
 
 export interface IWishlistItem {
@@ -17,22 +15,3 @@ export interface IWishlistResponse {
   data: IWishlistItem[];
   message?: string;
 }
-=======
-
-import { IProduct } from "./product.types";
-
-export interface IWishlistItem {
-  _id: string;
-  user_id: string;
-  product_id: IProduct;
-  createdAt: string;
-  updatedAt: string;
-  __v: number;
-}
-
-export interface IWishlistResponse {
-  success: boolean;
-  items: IWishlistItem[];
-  message?: string;
-}
->>>>>>> coop/join
