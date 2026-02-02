d190e2f8255b1b63d8026a1a475dfd261ca9e2b7
Lil-Adonis <adenugaib24@gmail.com>
2026-02-02 11:14:58 +0100

    landing page signed in state

--- app/page.tsx
+++ app/page.tsx
@@ -44,9 +44,13 @@ export default function Home() {
           <PopularProducts />
         </>
       )}
-      {!isAuthenticated && <Hero />}
-      <CooperativePlatform />
-      <WhyDAWSection />
+      {!isAuthenticated && (
+        <>
+          <Hero />
+          <CooperativePlatform />
+          <WhyDAWSection />
+        </>
+      )}
       {/* <MembershipPricing /> */}
       <Testimonials />
       <MarketplaceProducts />
